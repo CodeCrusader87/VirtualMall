b33967484b5033f7b01e4d5296d59b70d37074ed
Salih Zorer <zorer.salih@gmail.com>
2024-03-03 12:28:00 +0300

    feat: next-intl client provider

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { NextIntlClientProvider, useMessages } from 'next-intl';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,9 +17,12 @@ export default function RootLayout({
 	children: React.ReactNode;
 	params: { locale: string };
 }>) {
+	const messages = useMessages();
 	return (
 		<html lang={locale}>
-			<body className={inter.className}>{children}</body>
+			<NextIntlClientProvider messages={messages}>
+				<body className={inter.className}>{children}</body>
+			</NextIntlClientProvider>
 		</html>
 	);
 }
