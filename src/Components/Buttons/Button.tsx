813f53b705562b4464ca0d25092e506c406a6f83
Heaven <gabrielheavenlydawn@gmail.com>
2024-03-11 14:04:41 +0100

    feat: button component

--- src/Components/Buttons/Button.tsx
+++ src/Components/Buttons/Button.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 
 interface ButtonProps {
-  text: string;
-  icon?: React.ReactNode;
-  variant?: 'primary' | 'secondary';
+	text: string;
+	icon?: React.ReactNode;
+	variant?: 'primary' | 'secondary';
 }
 
 const Button: React.FC<ButtonProps> = ({ text, icon, variant = 'primary' }) => {
-  const validVariant = variant === 'secondary' ? 'secondary' : 'primary';
+	const validVariant = variant === 'secondary' ? 'secondary' : 'primary';
 
-  const buttonStyle = `button ${
-    validVariant === 'secondary' ? 'border border-green-800 rounded-xl text-lg font-semibold py-4 px-16 text-green-800' : 'bg-green-800 rounded-xl text-lg font-semibold py-4 px-16 text-white'
-  }`;
+	const buttonStyle = `button ${
+		validVariant === 'secondary'
+			? 'border border-green-800 rounded-xl text-lg font-semibold py-4 px-16 text-green-800'
+			: 'bg-green-800 rounded-xl text-lg font-semibold py-4 px-16 text-white'
+	}`;
 
-  return (
-    <button className={buttonStyle}>
-      {icon && <span className="button-icon mr-2">{icon}</span>}
-      {text}
-    </button>
-  );
+	return (
+		<button className={buttonStyle}>
+			{icon && <span className="button-icon mr-2">{icon}</span>}
+			{text}
+		</button>
+	);
 };
 
 export default Button;
