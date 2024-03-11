43743f5cac7527e183fa88162a7dd2a6c5e0a0f6
Heaven <gabrielheavenlydawn@gmail.com>
2024-03-11 15:57:43 +0100

    feat: button component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,9 @@
 import { useTranslations } from 'next-intl';
-import Button from '../../Components/Buttons/Button';
 export default function Home() {
 	const t = useTranslations();
-	return <div>{t('loginPage.title')}
-	<Button text="Login" variant='primary' />
-	<Button text="Sign up" variant='secondary' />
-
-	
-	</div>;
+	return (
+		<div>
+			{t('loginPage.title')}
+		</div>
+	);
 }
