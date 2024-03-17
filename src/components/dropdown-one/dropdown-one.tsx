42943d8e3f9d9ecc4e2f21d850476f019790c5bd
Salih Zorer <zorer.salih@gmail.com>
2024-03-17 13:44:18 +0300

    fix: drodpown-one style

--- src/components/dropdown-one/dropdown-one.tsx
+++ src/components/dropdown-one/dropdown-one.tsx
@@ -31,11 +31,11 @@ export default function DropdownOne({ buttonText, options }: DropdownOneProps) {
 				leaveFrom="transform opacity-100 scale-100"
 				leaveTo="transform opacity-0 scale-95"
 			>
-				<Menu.Items className="absolute left-0 mt-2 min-w-full origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+				<Menu.Items className="absolute left-0 mt-2 min-w-full origin-top-right divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
 					{options.map((option) => {
 						const Component = option.href ? Link : 'button';
 						return (
-							<Menu.Item key={option.text}>
+							<Menu.Item key={option.text} as="div" className="p-1">
 								{({ active }) => {
 									return (
 										<Component
